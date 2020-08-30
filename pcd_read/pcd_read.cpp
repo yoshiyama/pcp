55d748024fe63e77fe1ef275868aede65146ef8d
yoshiyama <yamamoto.ait@gmail.com>
2020-08-31 06:31:23 +0900

    commit again because failed error happened

---
+++ pcd_read/pcd_read.cpp
@@ -0,0 +1,34 @@
+// Software License Agreement (BSD License)
+
+// Point Cloud Library (PCL) - www.pointclouds.org
+// Copyright (c) 2009-2012, Willow Garage, Inc.
+// Copyright (c) 2012-, Open Perception, Inc.
+// Copyright (c) XXX, respective authors.
+
+// All rights reserved.
+
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int
+main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+    return (-1);
+  }
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from test_pcd.pcd with the following fields: "
+            << std::endl;
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    std::cout << "    " << cloud->points[i].x
+              << " "    << cloud->points[i].y
+              << " "    << cloud->points[i].z << std::endl;
+
+  return (0);
+}
