2b7bd6b5f98f27af70db8165bac3f3d295e2714d
yoshiyama <yamamoto.ait@gmail.com>
2020-09-10 14:50:03 +0900

    pcd_read fromPCLPointCloud2

---
+++ pcd_trans/matrix_transform4pcd/matrix_transform4pcd.cpp
@@ -0,0 +1,190 @@
+//
+// Software License Agreement (BSD License)
+
+// Point Cloud Library (PCL) - www.pointclouds.org
+// Copyright (c) 2009-2012, Willow Garage, Inc.
+// Copyright (c) 2012-, Open Perception, Inc.
+// Copyright (c) XXX, respective authors.
+
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met: 
+
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the copyright holder(s) nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <iostream>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+// This function displays the help
+void
+showHelp(char * program_name)
+{
+  std::cout << std::endl;
+  std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+  std::cout << "-h:  Show this help." << std::endl;
+}
+
+// This is the main function
+int
+main (int argc, char** argv)
+{
+
+  // Show help
+  if (pcl::console::find_switch (argc, argv, "-h") || pcl::console::find_switch (argc, argv, "--help")) {
+    showHelp (argv[0]);
+    return 0;
+  }
+
+  // Fetch point cloud filename in arguments | Works with PCD and PLY files
+  std::vector<int> filenames;
+  bool file_is_pcd = false;
+
+  filenames = pcl::console::parse_file_extension_argument (argc, argv, ".ply");
+
+  std::cout << "filenames=" << filenames.size() << std::endl;
+
+  if (filenames.size () != 1)  
+  {
+    filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
+    file_is_pcd = true;
+
+    // if (filenames.size () != 1) 
+    // {
+    //   showHelp (argv[0]);
+    //   return -1;
+    // } 
+    // else 
+    // {
+    //   file_is_pcd = true;
+    // }
+  }
+
+  // Load file | Works with PCD and PLY files
+  pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+
+  if (file_is_pcd) 
+  {
+    if (pcl::io::loadPCDFile (argv[filenames[0]], *source_cloud) < 0)  
+    {
+      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+      showHelp (argv[0]);
+      return -1;
+    }
+  } else {
+    if (pcl::io::loadPLYFile (argv[filenames[0]], *source_cloud) < 0)  {
+      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+      showHelp (argv[0]);
+      return -1;
+    }
+  }
+
+  /* Reminder: how transformation matrices work :
+
+           |-------> This column is the translation
+    | 1 0 0 x |  \
+    | 0 1 0 y |   }-> The identity 3x3 matrix (no rotation) on the left
+    | 0 0 1 z |  /
+    | 0 0 0 1 |    -> We do not use this line (and it has to stay 0,0,0,1)
+
+    METHOD #1: Using a Matrix4f
+    This is the "manual" method, perfect to understand but error prone !
+  */
+  Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
+
+  // Define a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
+  float theta = M_PI/4; // The angle of rotation in radians
+
+
+  transform_1 (0,0) = std::cos (theta);
+  transform_1 (0,1) = -sin(theta);
+  transform_1 (1,0) = sin (theta);
+  transform_1 (1,1) = std::cos (theta);
+  //    (row, column)
+
+  // Define a translation of 2.5 meters on the x axis.
+  transform_1 (0,3) = 2.5;
+
+  // Print the transformation
+  printf ("Method #1: using a Matrix4f\n");
+  std::cout << transform_1 << std::endl;
+
+  /*  METHOD #2: Using a Affine3f
+    This method is easier and less error prone
+  */
+  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+
+  // Define a translation of 2.5 meters on the x axis.
+  transform_2.translation() << 2.5, 0.0, 0.0;
+
+
+  // The same rotation matrix as before; theta radians around Z axis
+  transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+
+  // Print the transformation
+  printf ("\nMethod #2: using an Affine3f\n");
+  std::cout << transform_2.matrix() << std::endl;
+
+  // Executing the transformation
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+  // You can either apply transform_1 or transform_2; they are the same
+  pcl::transformPointCloud (*source_cloud, *transformed_cloud, transform_2);
+
+  // pcl::io::savePCDFileASCII ("trans.pcd", *transformed_cloud);
+  pcl::io::savePCDFileASCII (argv[2], *transformed_cloud);
+  // std::cerr << "Saved " << transformed_cloud.size () << " data points to test_pcd.pcd." << std::endl;
+
+  // Visualization
+  printf(  "\nPoint cloud colors :  white  = original point cloud\n"
+      "                        red  = transformed point cloud\n");
+  pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+
+   // Define R,G,B colors for the point cloud
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 255, 255, 255);
+  // We add the point cloud to the viewer and pass the color handler
+  viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
+  viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+
+  viewer.addCoordinateSystem (1.0, "cloud", 0);
+  viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
+  //viewer.setPosition(800, 400); // Setting visualiser window position
+
+  while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+    viewer.spinOnce ();
+  }
+
+  return 0;
+}
+
